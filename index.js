2968a0ecfb67c687e38dcdf46e1d764196226274
Ejoty123 <bradleydoesntgiveafox@gmail.com>
2023-05-07 21:06:46 -0400

    buttons

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ console.log (rand);
 console.log (guns.length);
 console.log(guns[rand]);
 document.getElementById("itsthegun").innerHTML = (guns[rand].name);
+
+//random button bullshit 
+
+var button = document.createElement('input');
+button.setAttribute('type', 'submit');
+button.setAttribute('ID', 'btnSendMailClone');
+button.setAttribute('value', 'Submit');
+button.setAttribute('onclick', 'btnSendMail_Click()');
+button.setAttribute('form', 'myform');
+document.body.appendChild(button);
+button.setAttribute("class", "btn btn-primary");
