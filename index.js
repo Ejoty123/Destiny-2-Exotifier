d1ff0e468a20588c2d4ab097fa5828debdd677e3
Ejoty123 <bradleydoesntgiveafox@gmail.com>
2023-05-07 20:29:57 -0400

    more guns better rand

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import guns from './guns.json' assert { type: 'json' };
 
-var rand = (Math.random() * guns.length);
+var rand = Math.floor(Math.random() * guns.length);
 console.log (rand);
 console.log (guns.length);
 console.log(guns[rand]);
